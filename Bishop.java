bd8d5cb550da1db60a5bae7b5d9cc8a106af89c1
Jonny Ulloa <127699789+jonnyulloa@users.noreply.github.com>
2025-02-11 15:18:22 -0700

    Add files via upload

---
+++ Bishop.java
@@ -0,0 +1,39 @@
+
+public class Bishop {
+    private static String pieceName;
+    private static String color;
+    private static char column;
+    private static char row;
+
+    Bishop(){}
+
+    Bishop(String pieceName, String color, char column, char row) {
+        this.pieceName = pieceName;
+        this.color = color;
+        this.column = column;
+        this.row = row;
+    }
+
+    public static void setColumn(char initColumn) {
+        column = initColumn;
+    }
+    public static void setRow(char initRow) {
+        row = initRow;
+    }
+
+    public static String getColor() {
+        return color;
+    }
+    public static char getColumn() {
+        return column;
+    }
+    public static char getRow() {
+        return row;
+    }
+    
+    // Derek, Jonathan, Ugyen
+    public static void verifyTarget(char toColumn, char toRow) {
+        boolean print = (Math.abs(toRow - row) == Math.abs(toColumn- column));                                                 
+        System.out.println(pieceName + " at " + column + row + (print ? " can " : " cannot ") + "move to " + toColumn + toRow);  
+    }
+}
