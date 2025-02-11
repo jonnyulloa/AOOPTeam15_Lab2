1900c062e8bfb361a865eebeff751f4ff39bd6b2
DIGamboa33 <derekgamboa33@gmail.com>
2025-02-11 15:25:13 -0700

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -4,9 +4,11 @@ public class Rook {
     private static char column;
     private static char row;
 
-    Rook(){}
+    public Rook(){
+        //Empty Constructor
+    }
 
-    Rook(String pieceName, String color, char column, char row) {
+    public Rook(String pieceName, String color, char column, char row) {
         this.pieceName = pieceName;
         this.color = color;
         this.column = column;
@@ -14,25 +16,25 @@ public class Rook {
     }
 
     public static void setColumn(char initColumn) {
-        column = initColumn;
+        column = initColumn; //set new initial column after asking the user
     }
     public static void setRow(char initRow) {
-        row = initRow;
+        row = initRow; //set new initial row after asking the user
     }
 
     public static String getColor() {
-        return color;
+        return color; //Getter for color of chess piece
     }
     public static char getColumn() {
-        return column;
+        return column; //Getter for column position of chess piece
     }
     public static char getRow() {
-        return row;
+        return row; //Getter for row position of chess piece
     }
     
     // Derek, Jonathan, Ugyen
     public static void verifyTarget(char toColumn, char toRow) {
-        boolean print = (column == toColumn|| column == toRow); 
+        boolean print = (column == toColumn|| column == toRow); //The Rook can only move vertically or horizontally, with no limits on squares
         System.out.println(pieceName + " at " + column + row + (print ? " can " : " cannot ") + "move to " + toColumn + toRow);  
     }
 }
