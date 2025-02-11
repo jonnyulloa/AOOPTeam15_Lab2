b78e5e74e1131235493d03feef190844b963fa62
DIGamboa33 <derekgamboa33@gmail.com>
2025-02-11 15:27:56 -0700

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -5,7 +5,9 @@ public class Queen {
     private static char column;
     private static char row;
 
-    Queen(){}
+    Queen(){
+        //Empty Constructor
+    }
 
     Queen(String pieceName, String color, char column, char row) {
         this.pieceName = pieceName;
@@ -15,26 +17,26 @@ public class Queen {
     }
 
     public static void setColumn(char initColumn) {
-        column = initColumn;
+        column = initColumn; //Set new column position after asking the user
     }
     public static void setRow(char initRow) {
-        row = initRow;
+        row = initRow; //Set new row position after asking the user
     }
 
     public static String getColor() {
-        return color;
+        return color; //Get color of chess piece
     }
     public static char getColumn() {
-        return column;
+        return column; //get column position of chess piece
     }
     public static char getRow() {
-        return row;
+        return row; //get row position of chess piece
     }
     
     // Derek, Jonathan, Ugyen
     public static void verifyTarget(char toColumn, char toRow) {
-        boolean print = (Math.abs(toRow - row) == Math.abs(toColumn- column) ||
-                        (column == toColumn) || (row == toRow)); 
+        boolean print = (Math.abs(toRow - row) == Math.abs(toColumn- column) ||  //The queen can move vertically, horizontally, and diagonally with no limits on the number of squares
+                        (column == toColumn) || (row == toRow)); //First check if the queen moved vertically, horizontally, then vertically
         System.out.println(pieceName + " at " + column + row + (print ? " can " : " cannot ") + "move to " + toColumn + toRow);  
     }
 }
