91a35c06c50d496622786359df04f4d1fe714b59
DIGamboa33 <derekgamboa33@gmail.com>
2025-02-11 15:29:50 -0700

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -5,7 +5,9 @@ public class Pawn {
     private static char column;
     private static char row;
 
-    Pawn(){}
+    Pawn(){
+        //Empty Constructor
+    }
 
     Pawn(String pieceName, String color, char column, char row) {
         this.pieceName = pieceName;
@@ -15,25 +17,25 @@ public class Pawn {
     }
 
     public static void setColumn(char initColumn) {
-        column = initColumn;
+        column = initColumn; //Set new column position after asking the user
     }
     public static void setRow(char initRow) {
-        row = initRow;
+        row = initRow; //Set new row position position after asking the user
     }
 
     public static String getColor() {
-        return color;
+        return color; //Get color of chess piece
     }
     public static char getColumn() {
-        return column;
+        return column; //get column position of chess piece
     }
     public static char getRow() {
-        return row;
+        return row; //get row position of chess piece
     }
     
     // Derek, Jonathan, Ugyen
     public static void verifyTarget(char toColumn, char toRow) {
-        boolean print = column == toColumn&& (toRow - row == 1 || (row == 2 && toRow == 4)); 
+        boolean print = column == toColumn&& (toRow - row == 1 || (row == 2 && toRow == 4)); //The pawn can only move one square forward or two squares forward if its the first move its made in the game
         System.out.println(pieceName + " at " + column + row + (print ? " can " : " cannot ") + "move to " + toColumn + toRow);  
     }
 }
