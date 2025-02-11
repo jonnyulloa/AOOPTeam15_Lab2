c2257b822ceb00ecbe69f45b7f256236719d1e51
DIGamboa33 <derekgamboa33@gmail.com>
2025-02-11 15:31:42 -0700

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -5,9 +5,11 @@ public class Knight {
     private static char column;
     private static char row;
 
-    Knight(){}
+    public Knight(){
+        //Empty Constructor
+    }
 
-    Knight(String pieceName, String color, char column, char row) {
+    public Knight(String pieceName, String color, char column, char row) {
         this.pieceName = pieceName;
         this.color = color;
         this.column = column;
@@ -15,26 +17,26 @@ public class Knight {
     }
 
     public static void setColumn(char initColumn) {
-        column = initColumn;
+        column = initColumn; //Set new column position
     }
     public static void setRow(char initRow) {
-        row = initRow;
+        row = initRow; //Set new row position
     }
 
     public static String getColor() {
-        return color;
+        return color; //get color of chess piece
     }
     public static char getColumn() {
-        return column;
+        return column; //get current column position of chess piece
     }
     public static char getRow() {
-        return row;
+        return row; //get row position
     }
     
     // Derek, Jonathan, Ugyen
     public static void verifyTarget(char toColumn, char toRow) {
         boolean print = (Math.abs(toRow - row) == 2 && Math.abs(toColumn- column) == 1) ||
-                        (Math.abs(toColumn- column) == 2 && Math.abs(toRow - row) == 1);                                                                              
+                        (Math.abs(toColumn- column) == 2 && Math.abs(toRow - row) == 1); ////The knight can move one square in one direction then two squares at an angle                                                                            
         System.out.println(pieceName + " at " + column + row + (print ? " can " : " cannot ") + "move to " + toColumn + toRow);  
     }
 }
