8ec47ff91325f527addaef626a9119a56d2a35a0
Jonny Ulloa <127699789+jonnyulloa@users.noreply.github.com>
2025-02-11 22:34:05 -0700

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -1,42 +1,40 @@
 
 public class Queen {
-    private static String pieceName;
-    private static String color;
-    private static char column;
-    private static char row;
+    private String pieceName;
+    private String color;
+    private char column;
+    private char row;
 
-    public Queen(){
-        //Empty Constructor
-    }
+    Queen(){}
 
-    public Queen(String pieceName, String color, char column, char row) {
+    Queen(String pieceName, String color, char column, char row) {
         this.pieceName = pieceName;
         this.color = color;
         this.column = column;
         this.row = row;
     }
 
-    public static void setColumn(char initColumn) {
-        column = initColumn; //Set new column position after asking the user
+    public void setColumn(char initColumn) {
+        column = initColumn;
     }
-    public static void setRow(char initRow) {
-        row = initRow; //Set new row position after asking the user
+    public void setRow(char initRow) {
+        row = initRow;
     }
 
-    public static String getColor() {
-        return color; //Get color of chess piece
+    public String getColor() {
+        return color;
     }
-    public static char getColumn() {
-        return column; //get column position of chess piece
+    public char getColumn() {
+        return column;
     }
-    public static char getRow() {
-        return row; //get row position of chess piece
+    public char getRow() {
+        return row;
     }
     
     // Derek, Jonathan, Ugyen
-    public static void verifyTarget(char toColumn, char toRow) {
-        boolean print = (Math.abs(toRow - row) == Math.abs(toColumn- column) ||  //The queen can move vertically, horizontally, and diagonally with no limits on the number of squares
-                        (column == toColumn) || (row == toRow)); //First check if the queen moved vertically, horizontally, then vertically
+    public void verifyTarget(char toColumn, char toRow) {
+        boolean print = (Math.abs(toRow - row) == Math.abs(toColumn- column) ||
+                        (column == toColumn) || (row == toRow)); 
         System.out.println(pieceName + " at " + column + row + (print ? " can " : " cannot ") + "move to " + toColumn + toRow);  
     }
 }
