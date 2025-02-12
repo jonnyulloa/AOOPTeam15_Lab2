fbfb0ee9bfe7dfe846ec6fe1095f5ea039fab583
Jonny Ulloa <127699789+jonnyulloa@users.noreply.github.com>
2025-02-11 22:35:53 -0700

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -1,41 +1,39 @@
 
 public class Bishop {
-    private static String pieceName;
-    private static String color;
-    private static char column;
-    private static char row;
+    private String pieceName;
+    private String color;
+    private char column;
+    private char row;
 
-    public Bishop(){
-        //Empty Constructor
-    }
+    Bishop(){}
 
-    public Bishop(String pieceName, String color, char column, char row) {
+    Bishop(String pieceName, String color, char column, char row) {
         this.pieceName = pieceName;
         this.color = color;
         this.column = column;
         this.row = row;
     }
 
-    public static void setColumn(char initColumn) {
-        column = initColumn; //set new column position
+    public void setColumn(char initColumn) {
+        column = initColumn;
     }
-    public static void setRow(char initRow) {
-        row = initRow; //set new row position
+    public void setRow(char initRow) {
+        row = initRow;
     }
 
-    public static String getColor() {
-        return color; //get color of chess piece
+    public String getColor() {
+        return color;
     }
-    public static char getColumn() {
-        return column; //get current column position
+    public char getColumn() {
+        return column;
     }
-    public static char getRow() {
-        return row; //get current row position
+    public char getRow() {
+        return row;
     }
     
     // Derek, Jonathan, Ugyen
-    public static void verifyTarget(char toColumn, char toRow) {
-        boolean print = (Math.abs(toRow - row) == Math.abs(toColumn- column)); //The Bishop can move vertically any number of squares                                              
+    public void verifyTarget(char toColumn, char toRow) {
+        boolean print = (Math.abs(toRow - row) == Math.abs(toColumn- column));                                                 
         System.out.println(pieceName + " at " + column + row + (print ? " can " : " cannot ") + "move to " + toColumn + toRow);  
     }
 }
