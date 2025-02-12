ba5d97b8642bf27832354e9969e27a9c846e69fd
Jonny Ulloa <127699789+jonnyulloa@users.noreply.github.com>
2025-02-11 22:34:50 -0700

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,42 +1,39 @@
-
 public class Knight {
-    private static String pieceName;
-    private static String color;
-    private static char column;
-    private static char row;
+    private String pieceName;
+    private String color;
+    private char column;
+    private char row;
 
-    public Knight(){
-        //Empty Constructor
-    }
+    Knight(){}
 
-    public Knight(String pieceName, String color, char column, char row) {
+    Knight(String pieceName, String color, char column, char row) {
         this.pieceName = pieceName;
         this.color = color;
         this.column = column;
         this.row = row;
     }
 
-    public static void setColumn(char initColumn) {
-        column = initColumn; //Set new column position
+    public void setColumn(char initColumn) {
+        column = initColumn;
     }
-    public static void setRow(char initRow) {
-        row = initRow; //Set new row position
+    public void setRow(char initRow) {
+        row = initRow;
     }
 
-    public static String getColor() {
-        return color; //get color of chess piece
+    public String getColor() {
+        return color;
     }
-    public static char getColumn() {
-        return column; //get current column position of chess piece
+    public char getColumn() {
+        return column;
     }
-    public static char getRow() {
-        return row; //get row position
+    public char getRow() {
+        return row;
     }
     
     // Derek, Jonathan, Ugyen
-    public static void verifyTarget(char toColumn, char toRow) {
-        boolean print = (Math.abs(toRow - row) == 2 && Math.abs(toColumn- column) == 1) ||
-                        (Math.abs(toColumn- column) == 2 && Math.abs(toRow - row) == 1); ////The knight can move one square in one direction then two squares at an angle                                                                            
+    public void verifyTarget(char toColumn, char toRow) {
+        boolean print = (Math.abs(toRow - row) == 2 && Math.abs(toColumn - column) == 1) ||
+                        (Math.abs(toColumn - column) == 2 && Math.abs(toRow - row) == 1);
         System.out.println(pieceName + " at " + column + row + (print ? " can " : " cannot ") + "move to " + toColumn + toRow);  
     }
 }
